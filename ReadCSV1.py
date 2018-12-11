f9fe56b6e8c80caf519ab3c40ebdcc0bd0c8ea6d
fumi0217 <30346086+fumi0217@users.noreply.github.com>
2018-12-11 23:05:44 +0900

    First commit

---
+++ ReadCSV1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 01:00:45 2018
+
+@author: fumi
+"""
+from time import time
+import glob
+import pandas as pd
+
+def main():
+    CSV = calcTime(readCSVs)
+    print('普通にpythonエンジンのread_csvメソッドを使って読み込むと{}秒かかりました。'.format(CSV['time']))       
+
+
+    
+def readCSVs():
+    #csvファイルのパスを取得
+    searchPath = r".\data\*.csv"
+    pathArray = glob.glob(searchPath)
+    
+    #pythonエンジンのread_csvを用いて、csvファイルを読み込む
+    for path in pathArray:
+        tmpCSV = pd.read_csv(path, encoding="SHIFT-JIS", index_col='DateTime', names=['Tid', 'Dealable', 'Pair', 'DateTime', 'Buy', 'Sell'], skiprows=1, engine="python")
+        
+        if 'CSV' in locals():
+            CSV = pd.concat([CSV, tmpCSV])
+            
+        else:
+            CSV = tmpCSV
+    return CSV
+
+
+#関数を引数に渡して、関数の実行時間を測るメソッドです。
+def calcTime(func):
+    start = time()
+    r = func()
+    return {'value' : r, 'time' : time()-start}
+    
+
+if __name__ == '__main__':
+    main()
